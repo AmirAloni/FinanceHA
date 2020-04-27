5bdb375d81e01352b121450034433a4b1326324b
amira <amiraloni7@gmail.com>
2020-04-27 18:29:28 +0300

    Ready for production

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   nickname: {
-    type: String,
+    type: String,  // Unique
     required: true,
     unique: true
   },
